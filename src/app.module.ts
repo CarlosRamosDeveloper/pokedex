73bdeb60f676db319f280fb4e5fa73cfd6f2d0f7
Carlos Ramos <carlosramosdev@protonmail.com>
2023-05-12 16:44:36 +0200

    Seed resource created

--- src/app.module.ts
+++ src/app.module.ts
@@ -4,6 +4,8 @@ import { join } from 'path';
 import { PokemonModule } from './pokemon/pokemon.module';
 import { MongooseModule } from '@nestjs/mongoose';
 import { CommonModule } from './common/common.module';
+import { SeedModule } from './seed/seed.module';
+import { SeedModule } from './seed/seed.module';
 
 @Module({
   imports: [
@@ -11,7 +13,8 @@ import { CommonModule } from './common/common.module';
     MongooseModule.forRoot("mongodb://localhost:27017/nest-pokemon"),
     ServeStaticModule.forRoot({
     rootPath: join(__dirname,"..","public")}),
-    CommonModule,        
+    CommonModule,
+    SeedModule,        
   ],
   controllers: [],
   providers: [],
