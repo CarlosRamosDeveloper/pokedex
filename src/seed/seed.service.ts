aed7f96d0cfb478e2af64bd3b56aaae1186c559e
Carlos Ramos <carlosramosdev@protonmail.com>
2023-05-16 09:16:42 +0200

    seed implemented

--- src/seed/seed.service.ts
+++ src/seed/seed.service.ts
@@ -1,8 +1,14 @@
 import { Injectable } from '@nestjs/common';
+import axios, { AxiosInstance } from 'axios';
 
 @Injectable()
 export class SeedService {
-  executeSeed(){
-    return "Seed executed";
+  private readonly axios: AxiosInstance = axios;
+  
+  async executeSeed(){
+    const {data} = await this.axios.get("https://pokeapi.co/api/v2/pokemon?limit=10");
+    console.log("Seed executed");
+
+    return data.results;
   }
 }
