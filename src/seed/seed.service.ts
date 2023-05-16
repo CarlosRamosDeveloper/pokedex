362998316b1dab2cc21e34cc74915b754835c7e0
Carlos Ramos <carlosramosdev@protonmail.com>
2023-05-16 17:58:34 +0200

    PokemonModule injected in seed

--- src/seed/seed.service.ts
+++ src/seed/seed.service.ts
@@ -1,10 +1,20 @@
 import { Injectable } from '@nestjs/common';
+import { InjectModel } from '@nestjs/mongoose';
+import { Model } from 'mongoose';
+
 import axios, { AxiosInstance } from 'axios';
+
+import { Pokemon } from 'src/pokemon/entities/pokemon.entity';
 import { PokeResponse } from './interfaces/poke-response.interface';
 
 @Injectable()
 export class SeedService {
   private readonly axios: AxiosInstance = axios;
+
+  constructor(
+    @InjectModel(Pokemon.name)
+    private readonly pokemonModel: Model<Pokemon>
+  ){}
   
   async executeSeed(){
     console.log("Gathering data");    
