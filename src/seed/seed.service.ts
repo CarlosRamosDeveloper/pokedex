76466a77e625ee5332a55c26ee60a0bc674abd3c
Carlos Ramos <carlosramosdev@protonmail.com>
2023-05-16 10:52:37 +0200

    Gathering data from pokeapi in executeSeed method

--- src/seed/seed.service.ts
+++ src/seed/seed.service.ts
@@ -1,12 +1,23 @@
 import { Injectable } from '@nestjs/common';
 import axios, { AxiosInstance } from 'axios';
+import { PokeResponse } from './interfaces/poke-response.interface';
 
 @Injectable()
 export class SeedService {
   private readonly axios: AxiosInstance = axios;
   
   async executeSeed(){
-    const {data} = await this.axios.get("https://pokeapi.co/api/v2/pokemon?limit=10");
+    console.log("Gathering data");    
+    const pokemonLimit:number = 10;
+    const POKEAPI_URL:string = `https://pokeapi.co/api/v2/pokemon?limit=${pokemonLimit}`;
+    const {data} = await this.axios.get<PokeResponse>(POKEAPI_URL);
+
+    data.results.forEach( ({name, url}) => {
+      const segments = url.split("/");
+      let no: number = +segments [ segments.length -2]
+
+      console.log(`${no}: ${name}`);
+    });
     console.log("Seed executed");
 
     return data.results;
