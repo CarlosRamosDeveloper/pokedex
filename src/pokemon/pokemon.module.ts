d80bcdc4428c57c0f75f4d00f43f20a27b5b3eb7
Carlos Ramos <carlosramosdev@protonmail.com>
2023-05-20 15:15:18 +0200

    ConfigModule implementation

--- src/pokemon/pokemon.module.ts
+++ src/pokemon/pokemon.module.ts
@@ -1,13 +1,17 @@
-import { Module } from '@nestjs/common';
-import { PokemonService } from './pokemon.service';
-import { PokemonController } from './pokemon.controller';
+import { ConfigModule } from '@nestjs/config';
 import { MongooseModule } from '@nestjs/mongoose';
+import { Module } from '@nestjs/common';
+
 import { Pokemon, PokemonSchema } from './entities/pokemon.entity';
+import { PokemonController } from './pokemon.controller';
+import { PokemonService } from './pokemon.service';
 
 @Module({
   controllers: [PokemonController],
   providers: [PokemonService],
-  imports: [MongooseModule.forFeature([
+  imports: [
+    ConfigModule,
+    MongooseModule.forFeature([
     {
       name: Pokemon.name,
       schema: PokemonSchema,
