e1578d573c0ad58cbaa67ff76d57fed6829e3fc7
Carlos Ramos <carlosramosdev@protonmail.com>
2023-05-20 13:04:33 +0200

    paginationDto updated

--- src/pokemon/pokemon.controller.ts
+++ src/pokemon/pokemon.controller.ts
@@ -3,6 +3,7 @@ import { PokemonService } from './pokemon.service';
 import { CreatePokemonDto } from './dto/create-pokemon.dto';
 import { UpdatePokemonDto } from './dto/update-pokemon.dto';
 import { ParseMongoIdPipe } from 'src/common/pipes/parse-mongo-id/parse-mongo-id.pipe';
+import { PaginationDto } from 'src/common/dto/pagination.dto';
 
 @Controller('pokemon')
 export class PokemonController {
@@ -14,8 +15,8 @@ export class PokemonController {
   }
 
   @Get()
-  findAll( @Query() queryParameters) {
-    console.log({queryParameters});
+  findAll( @Query() paginationDto: PaginationDto) {
+    console.log(paginationDto);
     return this.pokemonService.findAll();
   }
 
