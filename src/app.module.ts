9debfd72ac8c8d3394e859d978c73279a69311cd
Carlos Ramos <carlosramosdev@protonmail.com>
2023-05-20 14:34:21 +0200

    Creating environment variables

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,21 +1,28 @@
-import { Module } from '@nestjs/common';
-import { ServeStaticModule } from '@nestjs/serve-static';
 import { join } from 'path';
-import { PokemonModule } from './pokemon/pokemon.module';
+import { ConfigModule } from '@nestjs/config';
+import { Module } from '@nestjs/common';
 import { MongooseModule } from '@nestjs/mongoose';
+import { ServeStaticModule } from '@nestjs/serve-static';
+
 import { CommonModule } from './common/common.module';
+import { PokemonModule } from './pokemon/pokemon.module';
 import { SeedModule } from './seed/seed.module';
 
 @Module({
   imports: [
+    ConfigModule.forRoot(),      
     PokemonModule,
-    MongooseModule.forRoot("mongodb://localhost:27017/nest-pokemon"),
+    MongooseModule.forRoot(process.env.MONGODB),
     ServeStaticModule.forRoot({
     rootPath: join(__dirname,"..","public")}),
     CommonModule,
-    SeedModule,        
+    SeedModule,  
   ],
   controllers: [],
   providers: [],
 })
-export class AppModule {}
+export class AppModule {
+  constructor(){
+    console.log(process.env)
+  }
+}
